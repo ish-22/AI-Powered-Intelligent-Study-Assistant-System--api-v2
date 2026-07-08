bd0cdba71562064114a631fd804dfd43dfc05732
Ishan Chinthaka <ishanchinthaka2002@gmail.com>
2026-07-08 12:57:10 +0530

    admin panel

--- app/Models/DashboardStatistic.php
+++ app/Models/DashboardStatistic.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DashboardStatistic extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     public $incrementing = false;
     protected $keyType = 'string';
